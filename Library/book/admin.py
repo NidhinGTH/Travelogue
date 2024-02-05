0ea77f95a25d626b12f0043c4247eeec5b1e0b10
Nidhin Saji <84225145+NidhinGTH@users.noreply.github.com>
2024-02-05 10:51:13 +0530

    Delete Library directory

--- Library/book/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from book.models import book
-# Register your models here.
-admin.site.register(book)
--- Library/student/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from student.models import student
-from student.models import  CustomUser
-admin.site.register(student)
-admin.site.register(CustomUser)
