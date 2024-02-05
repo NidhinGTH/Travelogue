f1607c234ea685cbd106f0f2509f51346d06ac00
Nidhin Saji <84225145+NidhinGTH@users.noreply.github.com>
2024-02-05 10:51:36 +0530

    Delete movie directory

--- movie/movieshow/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from movieshow.models import movieshow
-
-# Register your models here.
-admin.site.register(movieshow)
--- movie/movieshow/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MovieshowConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movieshow'
